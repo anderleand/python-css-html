21ec224caff83bdf9b3ad87465734019fb01c098
anderleand <andersonleandro.desouza@gmail.com>
2021-03-18 22:28:02 +0000

    Atualização do __init__

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# python-css-html
+# Curso de Python com HTML e CSS da OCEAN - Flask
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Anderson Leandro'}
+    user = {'username': 'Anderson'}
     posts = [
         {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
-        {'author': {'username': 'Anderson'}, 'body': "Olá!"}
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
     ]
-
     return render_template("index.html", user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
+    return render_template("login.html" , title="Login")
